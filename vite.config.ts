3f9096c7cdd9ac03e8bb7e20910b38904c667c6f
ritusan <1323109714@qq.com>
2022-01-01 22:27:00 +0800

    use alias

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+const path = require("path")
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -15,7 +17,10 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
   ],
-  // resolve: {
-  //   extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
-  // }
+  resolve: {
+    // extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, '/src') }
+    ],  
+  }
 })
